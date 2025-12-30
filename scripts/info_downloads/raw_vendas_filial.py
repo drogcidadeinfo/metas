364af1fac56325545683e5ee8fe9769c0385c3e9
Drogaria Cidade <informatica@drogcidade.com.br>
2025-12-30 08:40:03 -0300

    updated raw_vendas_filial.py

--- scripts/info_downloads/raw_vendas_filial.py
+++ scripts/info_downloads/raw_vendas_filial.py
@@ -32,7 +32,7 @@ chrome_options = Options()
 chrome_options.add_argument("--headless")  
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
-chrome_options.add_argument("--window-size=1920,1080")  # Set dimensions
+# chrome_options.add_argument("--window-size=1920,1080")  # Set dimensions
 chrome_options.add_argument("--start-maximized")  # Maximize window
 
 prefs = {
