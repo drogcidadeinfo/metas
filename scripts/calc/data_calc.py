eca67886bba29e3a4e2a397bb2b43c79a6898bfe
Drogaria Cidade <informatica@drogcidade.com.br>
2026-03-02 00:35:42 -0300

    Update data_calc.py

--- scripts/calc/data_calc.py
+++ scripts/calc/data_calc.py
@@ -190,7 +190,7 @@ def populate_meta_gerente(sheet):
     df_merged["TKT MÉDIO_float"] = convert_br_to_float_series(df_merged["TKT MÉDIO"])
     df_merged["Custo Total_float"] = convert_br_to_float_series(df_merged["Custo Total"])
     df_merged["CMV_float"] = convert_br_to_float_series(df_merged["CMV"])
-    # df_merged["CMV_float"] = df_merged["CMV"]
+    # df_merged["CMV_float"] = df_merged["CMV"] * 100
     logging.info(df_merged["CMV_float"])
     
     # Calculate CMV % from VENDAS_FILIAL
