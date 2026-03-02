bdd052f55a42ecddb54b239eced63fff5303d3d6
Drogaria Cidade <informatica@drogcidade.com.br>
2026-03-02 00:21:28 -0300

    Update data_calc.py

--- scripts/calc/data_calc.py
+++ scripts/calc/data_calc.py
@@ -8,7 +8,7 @@ import math
 from google.oauth2.service_account import Credentials
 from googleapiclient.errors import HttpError
 from datetime import date, timedelta
-import calendarf
+import calendar
 
 # --------------------------------------------------
 # Config logging
