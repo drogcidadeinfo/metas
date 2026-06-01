67fd82a22cd5776da7fade9005bc0e2adfdb76b6
Drogaria Cidade <informatica@drogcidade.com.br>
2026-06-01 07:56:10 -0300

    Update raw_vendas_filial.py

--- scripts/info_downloads/raw_vendas_filial.py
+++ scripts/info_downloads/raw_vendas_filial.py
@@ -114,7 +114,7 @@ try:
     # log download start
     logging.info("Download has started.")
     # wait for download to complete
-    time.sleep(70)
+    time.sleep(30)
 
     # get the most recent downloaded file
     files = os.listdir(download_dir)
